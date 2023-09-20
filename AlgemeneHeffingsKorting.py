e5b2ecc0a5acc8ac34675f1359e4a31d26efdafc
SanityCS <evandokter@gmail.com>
2023-09-20 11:44:04 +0200

    Create AlgemeneHeffingsKorting.py

---
+++ AlgemeneHeffingsKorting.py
@@ -0,0 +1,9 @@
+inkomen = int(input("Voer belastbaar inkomen in: "))
+
+if inkomen < 22661:
+    ahk = (3070)
+elif inkomen in range(22661, 73031):
+    ahk = (3070 - 0.06095 * (inkomen - 22660))
+elif inkomen >= 73031:
+    ahk = (0)
+print ("Uw algemene heffingskorting is: %.2f " % float(ahk))
