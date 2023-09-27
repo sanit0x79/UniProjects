2044780babff98bb434ba1ee18b6ad40972a38b7
SanityCS <evandokter@gmail.com>
2023-09-27 12:57:38 +0200

    Smol little commit

--- pythonProjects/secondConverting2.py
+++ pythonProjects/secondConverting2.py
@@ -1,7 +1,9 @@
+seconds = int(input("How many seconds do you want to convert: "))
+              
 def amountOfSeconds(seconds):
     minutes, seconds = divmod(seconds,60)
     hours, minutes = divmod(minutes,60)
     days, hours = divmod(hours,24)
     return ("%d Days, %02d Hours, %02d Minutes, %02d seconds" % (days, hours, minutes, seconds))
 
-print(amountOfSeconds(100000))
+print(amountOfSeconds(seconds))
