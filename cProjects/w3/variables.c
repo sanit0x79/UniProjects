297380f9ab8395917a0728e14cd3ad365130db79
sanit0x79 <sanitycs@proton.me>
2024-05-22 09:19:55 +0200

    wasn't able to do much work on the train

---
+++ cProjects/w3/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int studentID = 15;
+	int studentAge = 23;
+	float studentFee = 30.75;
+	char studentGrade = 'A';
+
+	printf("The students ID is %d\n", studentID);
+	printf("The students age is %d\n", studentAge);
+	printf("The student fee is currently %.2f\n", studentFee);
+	printf("The students grade is %c\n", studentGrade);
+}
